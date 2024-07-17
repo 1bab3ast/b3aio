d668e86623f242af4ace239d91d96c5b80f8c1d8
Zach Leatherman <zachleat@users.noreply.github.com>
2024-07-17 16:37:39 -0500

    Update deps

--- api/avatar.js
+++ api/avatar.js
@@ -1,26 +1,42 @@
-import AvatarHtml from "./_avatarHtml.js";
+import AvatarHtml from "../avatar-html.js";
 
 const ONE_DAY = 60*60*24;
 const ONE_WEEK = ONE_DAY*7;
 
 const IMAGE_WIDTH = 60;
 const IMAGE_HEIGHT = 60;
-const IMAGE_TTL = 1 * 7 * 24 * 60 * 60; // 1 week in seconds
 const FALLBACK_IMAGE_FORMAT = "png";
+
+function isFullUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch(e) {
+    // invalid url OR local path
+    return false;
+  }
+}
+
+function getEmptyImageResponse(errorMessage) {
+  // We need to return 200 here or Firefox won’t display the image
+  // empty svg
+  return new Response(`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${IMAGE_WIDTH}" height="${IMAGE_HEIGHT}" aria-hidden="true" focusable="false"></svg>`, {
+    status: 200,
+    headers: {
+      "content-type": "image/svg+xml",
+      "x-11ty-error-message": errorMessage,
+      "cache-control": `public, s-maxage=${ONE_WEEK}, stale-while-revalidate=${ONE_DAY}`,
+    }
+  })
+}
 
 export async function GET(request, context) {
   // e.g. /https%3A%2F%2Fwww.11ty.dev%2F/
   let requestUrl = new URL(request.url);
   let [url] = requestUrl.pathname.split("/").filter(entry => !!entry);
 
-  if(!url || url?.endsWith("favicon.ico")) {
-    return new Response("{}", {
-      status: 200,
-      headers: {
-        "content-type": "application/json",
-        "cache-control": `public, s-maxage=${ONE_WEEK}, stale-while-revalidate=${ONE_DAY}`
-      }
-    });
+  if(url?.endsWith("favicon.ico")) {
+    return getEmptyImageResponse("");
   }
 
   url = decodeURIComponent(url);
@@ -28,6 +44,11 @@ export async function GET(request, context) {
   try {
     // output to Function logs
     console.log("Fetching", url);
+
+    // short circuit circular requests
+    if(isFullUrl(url) && (new URL(url)).hostname.endsWith(".indieweb-avatar.11ty.dev")) {
+      return getEmptyImageResponse("Circular request");
+    }
 
     let avatar = new AvatarHtml(url);
     await avatar.fetch();
@@ -44,16 +65,7 @@ export async function GET(request, context) {
     });
   } catch (error) {
     console.log("Error", error);
-
-    // We need to return 200 here or Firefox won’t display the image
-    // empty svg
-    return new Response(`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="${IMAGE_WIDTH}" height="${IMAGE_HEIGHT}" aria-hidden="true" focusable="false"></svg>`, {
-      headers: {
-        "content-type": "image/svg+xml",
-        "x-11ty-error-message": error.message,
-        "cache-control": `public, s-maxage=${ONE_WEEK}, stale-while-revalidate=${ONE_DAY}`,
-      }
-    });
+    return getEmptyImageResponse(error.message);
   }
 }
 
